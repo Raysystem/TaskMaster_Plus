639aaf3a0b9ad755bc26dfce95ea19476d23cc11
Raysystem <rayproducoes7@gmail.com>
2024-04-13 10:50:58 -0300

    feat: deploy3

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 3000
-console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
